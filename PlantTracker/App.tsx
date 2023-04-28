40f1839c4cbac64290ace10bc003804a9d417c41
Gelczynski <jakub.gelczynski@studenci.collegiumwitelona.pl>
2023-04-28 19:55:13 +0200

    first login view

--- PlantTracker/App.tsx
+++ PlantTracker/App.tsx
@@ -1,117 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
+  Pressable,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import AppStyles from './src/AppStyles';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.content}>
+      <View style={styles.content}>
+        <View style={styles.logo}>
+          <Text style={styles.header}>Plant Tracker</Text>
         </View>
-      </ScrollView>
+
+        <Pressable style={[styles.button, { backgroundColor: 'black' }]}>
+          <Text style={[AppStyles.text, { color: 'white' }]}>Continue with Apple</Text>
+        </Pressable>
+
+        <Pressable style={[styles.button, { backgroundColor: '#1877f2' }]}>
+          <Text style={[AppStyles.text, { color: 'white' }]}>Continue with Facebook</Text>
+        </Pressable>
+
+        <Pressable style={[styles.button, { backgroundColor: 'white' }]}>
+          <Text style={[AppStyles.text, { color: 'black' }]}>Continue with Google</Text>
+        </Pressable>
+
+        <Text style={[AppStyles.text, {margin: 10}]}>or</Text>
+
+        <Pressable style={[styles.button, { backgroundColor: '#3ddc84' }]}>
+          <Text style={[AppStyles.text, { color: 'black' }]}>Download Android App</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  content: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
   },
-  sectionTitle: {
+  logo: {
+    margin: 20,
+  },
+  header: {
+    ...AppStyles.text,
     fontSize: 24,
-    fontWeight: '600',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    padding: 15,
+    textAlign: 'center',
+    borderRadius: 10,
+    margin: 10,
+    alignItems: 'center',
+    width: 500,
   },
 });
 
